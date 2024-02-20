41bcc9e712f7be959520e60e34c4353eb5f275cd
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2024-02-20 11:52:44 +0530

    new changes

--- src/configurations/search/search/StyleSearch.jsx
+++ src/configurations/search/search/StyleSearch.jsx
@@ -1,7 +1,7 @@
 
 // StyleSearch.js
 const StyleSearch = {
-  search1: "flex items-center text-gray-700 mt-26 text-sm px-3 py-1 w-64 border border-gray-300",
+  search1: "flex items-center text-gray-700 mt-26 text-sm px-3 py-1 w-64 border-2 rounded-md border-gray-200",
   iconSize: "1.2em",
   input: {
     border: 'none',  
