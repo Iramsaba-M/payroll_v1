fe6ceca37878f4a1504c66a8eecd3f93bd57d32a
Iramsaba mirjankar <mirjankar.iramsaba@infokalash.com>
2024-02-21 11:35:53 +0000

    Update GetAPI.jsx

--- src/api/GetAPI.jsx
+++ src/api/GetAPI.jsx
@@ -1,20 +1,10 @@
-
-const baseUrl = 'http://192.168.0.120:8000';
-// const baseUrl = 'http://localhost:8000';
-const baseUrl2 = 'http://192.168.0.106:8001';
 // const baseUrl = 'http://localhost:3000';
-const baseUrl3 = 'http://192.168.0.120:8000';
 
+const baseUrl = 'http://192.168.0.121:8000';
 
 
 export const getApiUrl = (endpoint) => {
   return `${baseUrl}/${endpoint}`;
 };
 
-export const getApiUrl2 = (endpoint) => {
-  return `${baseUrl2}/${endpoint}`;
-};
 
-export const getApiUrl3 = (endpoint) => {
-  return `${baseUrl3}/${endpoint}`;
-};
