2d5f2197934636401da8dbe39ce2c0adcee18be6
Shashank baikadi <baikadi.shashank@infokalash.com>
2024-02-22 12:47:06 +0530

    changed Modal Styles

--- src/components/form/Formfields/modal/ModalStyles.jsx
+++ src/components/form/Formfields/modal/ModalStyles.jsx
@@ -6,13 +6,13 @@ const ModalStyles = {
     // header: 'text-lg leading-6 font-medium text-blue-900 ',
     // CrossButton: 'ml-2 mb-5 h-6 w-7  rounded-md hover:bg-gray-200 ',
     // standard: 'mb-5 text-3xl font-bold text-blue-600 align-items-center ',
-    MainDiv: ' ml-[20vh] fixed z-10 inset-0 overflow-y-auto translate-y-[20vh] ',
-    ModalBody: 'm-4 sm:m-auto w-full sm:w-1/2 bg-white rounded-lg shadow-lg px-4 pt-5 pb-4 sm:p-6 sm:pb-4  h-80 ',
-    Header: ' h-[35vh]  flex justify-center items-center   mt-',
+    MainDiv: ' ml-[20vh] fixed z-10 inset-0 overflow-y-auto translate-y-[30vh]  ',
+    ModalBody: 'm-4 sm:m-auto w-60 sm:w-1/2 bg-white rounded-lg shadow-2xl border px-4 pt-5 pb-4 sm:p-6 sm:pb-4  h-60  sm:w-[70vh] ',
+    Header: ' h-[36vh]  flex justify-center items-center  ',
     header: 'text-lg leading-6 font-medium text-blue-900  ',
-    CrossButton: 'ml- mb-5 mb-[38vh] translate-x-[12vh] rounded-md translate-y-5',
-    standard: 'mb- text-3xl font-bold text-blue-600 translate-x-[18vh]  ',
-    standardline: ' mt-10 -translate-x-[13vh] text-3xl font-bold text-blue-600   ',
+    CrossButton: 'ml- mb-5 mb-[39vh] rounded-md translate-y-5',
+    standard: 'mb- text-3xl font-bold text-blue-600 translate-x-[18vh] mb-28  ',
+    standardline: ' mt-10 -translate-x-[12vh] text-3xl font-bold text-blue-600 mb-28  ',
     
   };
 
