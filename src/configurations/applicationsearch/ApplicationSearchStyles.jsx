cc9ada7a055af385a1eaf8dc97decbb6a408002d
Vaishnav konnur <konnur.vaishnavi@infokalash.com>
2024-02-29 12:23:24 +0530

    application search

--- src/configurations/applicationsearch/ApplicationSearchStyles.jsx
+++ src/configurations/applicationsearch/ApplicationSearchStyles.jsx
@@ -1,13 +1,9 @@
 // ApplicationSearchStyles.js
 const ApplicationSearchStyles = {
-  search1: "flex items-center text-gray-700 bg-gray-50 mt-4 text-sm px-3 py-1 w-64 border-1 rounded-sm bg-gray-200", // Style for the search container
-  input: {
-    backgroundColor: 'transparent',
-    color: 'black',
-    border: 'none',  
-    outline: 'none', 
-    width: '100%',   
-  },
+  // Other styles...
+  searchContainer: 'relative', // Keep existing styles for search container
+  input: 'bg-transparent outline-none border-b border-gray-400 py-2 pl-8 pr-4 w-full text-gray-800 placeholder-gray-500', // Keep existing styles for input
+  suggestionList: 'absolute left-0 w-full bg-white border border-gray-400 mt-2 rounded-md shadow-md z-10', // Adjust styles for suggestion list
 };
 
 export default ApplicationSearchStyles;
