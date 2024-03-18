d1e9f9156098f27f79aa3301e88ca4f307cb791e
Iramsaba Mirjankar <mirjankar.iramsaba@infokalash.com>
2024-03-18 11:58:43 +0530

    run payroll

--- src/pages/Run Payroll/RunPayrollcontent.jsx
+++ src/pages/Run Payroll/RunPayrollcontent.jsx
@@ -1,89 +1,110 @@
-import { MdOutlineEdit } from 'react-icons/md';
-import TableStyle2 from '../../configurations/table2/Tablestyle2';
-export const Button1Content = [
-    { label: ' ',  style: 'buttonStylew' , type:"submit"},
-    
+export const RunPayrolltableContent = [
+ 
+    {
+      name: "employee_name",
+      label: "EMPLOYEE NAME",
+      dataType: "string",
+      cssClass: "datastyle",
+      clmncss : "payrollstyle3"
+  },
+  {
+      name: "designation",
+      label: "DESIGNATION",
+      dataType: "string",
+      cssClass: "payrolldatastyle4",
+      clmncss : "payrollstyle3"
+  },
+  {
+      name: "attendance",
+      label: "Attendance",
+      dataType: "string",
+      cssClass: "payrolldatastyle4",
+      clmncss : "payrollstyle3"
+  },
+  {
+    name: "gross_salary",
+    label: "Gross Salary",
+    dataType: "string",
+    cssClass: "payrolldatastyle4",
+    clmncss : "payrollstyle3"
+},
+{
+    name: "net_pay",
+    label: "Net Pay",
+    dataType: "string",
+    cssClass: "payrolldatastyle4",
+    clmncss : "payrollstyle3"
+},
+{
+    name: "payroll_status",
+    label: "PAYROLL STATUS",
+    dataType: "string",
+    cssClass: "payrolldatastyle4",
+    clmncss : "payrollstyle4",
+    statusStyles: {
+      'Finalized': 'payrolldatastyle3', 
+      'Finalize now': 'payrolldatastyle5', 
+    },
+},
+  
   ];
 
-  export const tableContent2 = [
-    {
-        name: "employee_name",
-        label: "EMPLOYEE NAME",
-        dataType: "string",
-        cssClass: "datastyle",
-        clmncss : "style4"
-    },
-    {
-        name: "designation",
-        label: "DESIGNATION",
-        dataType: "string",
-        cssClass: "datastyle1",
-        clmncss : "style4"
-    },
-    {
-        name: "employee_id",
-        label: "EMP ID",
-        dataType: "string",
-        cssClass: "datastyle1",
-        clmncss : "style4"
-    },
-    {
-        name: "PAYSLIPS",
-        label: "PAYSLIPS",
-        dataType: "string",
-        cssClass: "datastyle1",
-        clmncss : "style4"
-    },
-    
-    {
-      name: "DOWNLOAD",
-      label: "DOWNLOAD",
-      dataType: "string",
-      cssClass: "datastyle1",
-      clmncss : "style4"
-    
-    }
-    
-  ]
+  export const statusButtons=[{
+    label:"status"
+  }]
+  export const finalizeButtons=[
+  { label: 'Bulk Finalize', style: 'payrollbuttonStyle1' },
+]
+export const ApproveandProcess=[
 
-  export const tableContent3 = [
-    {
-        name: "netpay",
-        label: "EMPLOYEE NET PAY",
-        dataType: "string",
-        cssClass: "datastyle2",
-        clmncss : "style1"
-    },
-    {
-        name: "month",
-        label: "MONTH",
-        dataType: "string",
-        cssClass: "datastyle1",
-        clmncss: "style1",
-      },
-      {
-        name: "payment-date",
-        label: "PAYMENT DATE",
-        dataType: "string",
-        cssClass: "datastyle1",
-        clmncss: "style1",
-      },
-      {
-        name: "total-employee",
-        label: "NO OF EMPLOYEES",
-        dataType: "string",
-        cssClass: "datastyle2",
-        clmncss: "style1",
-      },
-      {
-        name: "action",
-        label: " ACTION",
-        dataType: "string",
-        cssClass: "datastyle4",
-        clmncss: "style1",
-        actionStyles: {
-          'Review': 'datastyle3', // Specify the class name directly
-          'Processed': 'datastyle4', // Specify the class name directly
-        },    
-      },
-    ];
+  { label: 'Approve & Process Payroll', style: 'payrollbuttonStyle1' },
+]
+export const PrintPayslip=[
+
+  { label: 'Print Payslips', style: 'payrollbuttonStyle1' },
+]
+
+export const cardContent = [
+  {
+    heading: 'Taxes & Deduction',
+    multivalue: [
+      { heading: 'PF', name: 'pf' },
+      { heading: 'ESIC', name: 'esic' },
+      { heading: 'PT', name: 'pt' }
+    ],
+    card: 'payrollstyle2',
+    headstyle: 'payrollheading3'
+  },
+];
+export const cardContent2 = [
+  {
+    heading: 'PAYROLL EXPENSE',
+    card: 'payrollstyle1',
+    contentstyle: 'payrollcontent',
+    headstyle: 'payrollheading'
+  },
+];
+export const cardContent3 = [
+  {
+    heading: 'EMPLOYEE NET PAY',
+    card: 'payrollstyle3',
+    contentstyle: 'payrollcontent',
+    headstyle: 'payrollheading'
+  },
+];
+export const cardContent4 = [
+  {
+    heading: 'TOTAL EMPLOYEES ',
+    card: 'payrollstyle1',
+    contentstyle: 'payrollcontent2',
+    headstyle: 'payrollheading2'
+  },
+];
+export const cardContent5 = [
+  {
+    heading: 'PAYABLE DAYS',
+    card: 'payrollstyle3',
+    contentstyle: 'payrollcontent2',
+    headstyle: 'payrollheading2'
+  },
+];
