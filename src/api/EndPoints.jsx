5c2b1ba722237144d3337149254a7e266bd3822b
Iramsaba Mirjankar <mirjankar.iramsaba@infokalash.com>
2024-04-01 12:27:25 +0530

    changed

--- src/api/EndPoints.jsx
+++ src/api/EndPoints.jsx
@@ -1,6 +1,8 @@
 
-// export const EMP_API = 'employee/basic-details';
-export const EMP_API = 'basicdetail';
+//Employee Registeration
+export const EMP_API = 'employee/basic-details';
+
+export const BASIC_DETAILS_API_Get = 'employee/basic-details';
 
 export const CARDS_API = 'employee/total_ctc_and_employees';
 
@@ -20,7 +22,6 @@ export const Import_GET_API = 'employee/download_template';
 
 export const Import_UPLOAD__GET_API = 'employee/upload_and_process';
 
-
 export const BasicDetails_export = 'employee/export/basic-details/';
 
 export const SalaryDetails_export = 'employee/export/salary-details';
@@ -29,14 +30,14 @@ export const BankDetails_export = 'employee/export/bank-details';
 
 export const Additionaldetails_export = 'employee/export/additional-details';
 
+// Home and Report 
 export const Home_and_Reportdata ='reports/month-data'
 
 export const Home_and_Report_BarGraphdata ='reports/graph-data'
 
-
+//Runpayroll
 export const Runpayroll ='Runpayroll';
 
-// export const payslips ='fetch_employee_data';
 export const mypayslip ='mypayslip';
 
 export const payslips ='Payslip';
