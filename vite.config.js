f964450d3be59990cadb1d7cdd555fb678a0eb48
Vinaykumar <kumar.vinay@infokalash.com>
2024-04-17 10:47:31 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
-
- viteconfig.js
-
 // vite.config.js
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-plugins: [react()],
-server: {
-host: '0.0.0.0',
-},
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
 });
-
-
-// export default {
-// server: {
-// port: 5173, // Make sure this matches the port your application is configured to use
-// },
-// };
