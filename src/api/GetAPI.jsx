5a4ea970656ebcd23f451392decf614245e292b5
IK Admin <admin@example.com>
2024-04-23 13:52:38 +0000

    Update GetAPI.jsx

--- src/api/GetAPI.jsx
+++ src/api/GetAPI.jsx
@@ -1,6 +1,6 @@
 // const baseUrl = 'http://localhost:3000';
 // 
-const baseUrl = 'http://192.168.0.131:8000';
+const baseUrl = 'http://192.168.0.100:8000';
 
 
 export const getApiUrl = (endpoint) => {
