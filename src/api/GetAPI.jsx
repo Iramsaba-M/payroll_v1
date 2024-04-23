0ca269e3c72b377d17dd20f51274a93a7f7b33fa
IK Admin <admin@example.com>
2024-04-23 13:59:59 +0000

    Update GetAPI.jsx

--- src/api/GetAPI.jsx
+++ src/api/GetAPI.jsx
@@ -1,7 +1,8 @@
 // const baseUrl = 'http://localhost:3000';
 // 
-const baseUrl = 'http://192.168.0.100:8000';
+// const baseUrl = 'http://192.168.0.100:8000';
 
+const baseUrl = 'http://python-app-image:8000';
 
 export const getApiUrl = (endpoint) => {
   return `${baseUrl}/${endpoint}`;
