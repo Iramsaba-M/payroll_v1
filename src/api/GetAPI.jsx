0518c1a07e0ab97be2451585a4ffb77be03b3dca
Amit Joshi <joshi.amit@infokalash.com>
2024-05-15 07:52:02 +0000

    Update GetAPI.jsx

--- src/api/GetAPI.jsx
+++ src/api/GetAPI.jsx
@@ -1,6 +1,6 @@
 // const baseUrl = 'http://localhost:3000';
 // 
- const baseUrl = 'http://192.168.0.202:8000';
+ const baseUrl = 'http://192.168.0.205:8000';
 
 //const baseUrl = 'http://python-app-image:8000';
 
